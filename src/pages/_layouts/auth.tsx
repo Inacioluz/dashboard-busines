637b5012d8f879b041b91ffbe6dd5ef6bec0a3a4
Inacio Silva <joseinaciolds@gmail.com>
2024-01-23 10:19:06 -0300

    tela login ok

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,19 @@
+import { Landmark } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticaçãp</h1>
-
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-foreground ">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Landmark className="h-5 w-5" />
+          <span className="font-semibold">dash.busines</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy dash.busunes - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
