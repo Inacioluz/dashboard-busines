34444b004a35a0caddf0ac05cd2aba0d1be468ec
Inacio Silva <joseinaciolds@gmail.com>
2024-01-24 21:53:10 -0300

    usando soner para menssagem de altenticação

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -22,6 +23,8 @@ export function SignIn() {
   async function handleSignIn(data: SignInform) {
     console.log(data)
     await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('enviamenos um link de alternticação para seu e-mail')
   }
 
   return (
