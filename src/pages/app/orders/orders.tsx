747014a95024ae35c3774a53ec688f9e8d694b8b
Inacio Silva <joseinaciolds@gmail.com>
2024-01-27 00:01:29 -0300

    contruindo listagem de pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,6 +1,16 @@
+import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 export function Orders() {
   return (
@@ -15,7 +25,64 @@ export function Orders() {
           <Input placeholder="Nome do cliente" className="h-8 w-[328px]" />
         </form>
         <div className="rounded-md border">
-            
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell>
+                      <Button variant="outline" size="xs">
+                        <Search className="h-3 w-3" />
+                        <span className="sr-only">detelhes do pedido</span>
+                      </Button>
+                    </TableCell>
+                    <TableCell className="font-nono text-xs font-medium">
+                      iuovbsergbeorh0sd
+                    </TableCell>
+                    <TableCell className="text-nuted-foreground">
+                      há 20 minutos
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        <span className="h-2 w-2 rounded-full bg-slate-400" />
+                        <span className="font-medium text-muted-foreground">
+                          Pendente
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell className="font-medium">
+                      Inacio Luz Silva
+                    </TableCell>
+                    <TableCell className="font-medium">R$ 199,99</TableCell>
+                    <TableCell>
+                      <Button className="outline" size="xs">
+                        <ArrowRight className="mr-2 h-3 w-3" />
+                        Aprovar
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                      <Button className="ghost" size="xs">
+                        <X className="mr-2 h-3 w-3" />
+                        Cancelar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </>
