e77cbb0f02c47dac35332f7c5c26b4bf741e0a4a
Inacio Silva <joseinaciolds@gmail.com>
2024-02-07 11:37:23 -0300

    cadastro de reastaurants

---
+++ src/api/register-restaurant.ts
@@ -0,0 +1,22 @@
+import { api } from '@/lib/axios'
+
+export interface RegisterRestaurantBody {
+  email: string
+  restaurantName: string
+  phone: string
+  managerName: string
+}
+
+export async function registerRestaurant({
+  restaurantName,
+  managerName,
+  email,
+  phone,
+}: RegisterRestaurantBody) {
+  await api.post('/restaurants', {
+    restaurantName,
+    managerName,
+    email,
+    phone,
+  })
+}
