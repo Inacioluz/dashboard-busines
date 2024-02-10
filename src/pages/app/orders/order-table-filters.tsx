f61be1e7a95c124d09d1a8208d4b8f27a37e7ee6
Inacio Silva <joseinaciolds@gmail.com>
2024-02-09 22:08:23 -0300

    remove filters

--- src/pages/app/orders/order-table-filters.tsx
+++ src/pages/app/orders/order-table-filters.tsx
@@ -20,36 +20,39 @@ const orderFiltersSchema = z.object({
   status: z.string().optional(),
 })
 
-type OrderFilterSchema = z.infer<typeof orderFiltersSchema>
+type OrderFiltersSchema = z.infer<typeof orderFiltersSchema>
 
 export function OrderTableFilters() {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const orderId = searchParams.get('orderId')
-  const customerName = searchParams.get('costumerName')
+  const customerName = searchParams.get('customerName')
   const status = searchParams.get('status')
 
-  const { register, handleSubmit, control } = useForm<OrderFilterSchema>({
-    resolver: zodResolver(orderFiltersSchema),
-    defaultValues: {
-      orderId: orderId ?? '',
-      customerName: customerName ?? '',
-      status: status ?? 'all',
-    },
-  })
+  const { register, handleSubmit, control, reset } =
+    useForm<OrderFiltersSchema>({
+      resolver: zodResolver(orderFiltersSchema),
+      defaultValues: {
+        orderId: orderId ?? '',
+        customerName: customerName ?? '',
+        status: status ?? 'all',
+      },
+    })
 
-  function handleFilter({ orderId, customerName, status }: OrderFilterSchema) {
+  function handleFilter({ customerName, orderId, status }: OrderFiltersSchema) {
     setSearchParams((state) => {
       if (orderId) {
         state.set('orderId', orderId)
       } else {
         state.delete('orderId')
       }
+
       if (customerName) {
         state.set('customerName', customerName)
       } else {
         state.delete('customerName')
       }
+
       if (status) {
         state.set('status', status)
       } else {
@@ -61,6 +64,24 @@ export function OrderTableFilters() {
       return state
     })
   }
+
+  function handleClearFilters() {
+    setSearchParams((state) => {
+      state.delete('orderId')
+      state.delete('customerName')
+      state.delete('status')
+      state.set('page', '1')
+
+      return state
+    })
+
+    reset({
+      orderId: '',
+      customerName: '',
+      status: 'all',
+    })
+  }
+
   return (
     <form
       onSubmit={handleSubmit(handleFilter)}
@@ -108,7 +129,12 @@ export function OrderTableFilters() {
         <Search className="mr-2 h-4 w-4" />
         Filtrar resultados
       </Button>
-      <Button variant="outline" size="xs" type="button">
+      <Button
+        onClick={handleClearFilters}
+        variant="outline"
+        size="xs"
+        type="button"
+      >
         <X className="mr-2 h-4 w-4" />
         Remover filtros
       </Button>
