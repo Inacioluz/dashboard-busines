55ddf832cdc42cbb26ef3bf2194e9ab14e8e1be9
Inacio Silva <joseinaciolds@gmail.com>
2024-02-18 09:39:30 -0300

    usando Spies

---
+++ src/components/pagination.spec.tsx
@@ -0,0 +1,46 @@
+import '@testing-library/jest-dom'
+
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import sinon from 'sinon'
+
+import { Pagination } from './pagination'
+
+const onPageChangeCallback = sinon.spy()
+
+describe('Pagination', () => {
+  it('should display the right amount of pages and results', () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onPageChange={() => {}}
+      />,
+    )
+
+    expect(wrapper.getByText('Página 1 de 20')).toBeInTheDocument()
+    expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
+  })
+
+  it('should be able to navigate to the next page', async () => {
+    const user = userEvent.setup()
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Próxima página',
+    })
+
+    await user.click(nextPageButton)
+  })
+})
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -8,7 +8,7 @@ import React from 'react'
 
 import { Button } from './ui/button'
 
-export interface PaginationProps {
+interface PaginationProps {
   pageIndex: number
   totalCount: number
   perPage: number
@@ -24,56 +24,54 @@ export function Pagination({
   const pages = Math.ceil(totalCount / perPage) || 1
 
   return (
-    <>
-      <div className="flex items-center justify-between">
-        <span className="text-sm text-muted-foreground">
-          Total de {totalCount} item(s)
-        </span>
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
 
-        <div className="flex items-center gap-6 lg:gap-8">
-          <div className="text-sm font-medium">
-            Página {pageIndex + 1} de {pages}
-          </div>
-          <div className="flex items-center gap-2">
-            <Button
-              onClick={() => onPageChange(0)}
-              variant="outline"
-              className="h-8 w-8 p-0"
-              disabled={pageIndex === 0}
-            >
-              <ChevronsLeft className="h-4 w-4" />
-              <span className="sr-only">Primeira Página</span>
-            </Button>
-            <Button
-              onClick={() => onPageChange(pageIndex - 1)}
-              variant="outline"
-              className="h-8 w-8 p-0"
-              disabled={pageIndex === 0}
-            >
-              <ChevronLeft className="h-4 w-4" />
-              <span className="sr-only">Próxima Página</span>
-            </Button>
-            <Button
-              onClick={() => onPageChange(pageIndex + 1)}
-              variant="outline"
-              className="h-8 w-8 p-0"
-              disabled={pages < pageIndex + 1}
-            >
-              <ChevronRight className="h-4 w-4" />
-              <span className="sr-only">Página anterior</span>
-            </Button>
-            <Button
-              onClick={() => onPageChange(pages - 1)}
-              variant="outline"
-              className="h-8 w-8 p-0"
-              disabled={pages < pageIndex + 1}
-            >
-              <ChevronsRight className="h-4 w-4" />
-              <span className="sr-only">Última Página</span>
-            </Button>
-          </div>
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
+        <div className="flex items-center gap-2">
+          <Button
+            onClick={() => onPageChange(0)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === 0}
+          >
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira página</span>
+          </Button>
+          <Button
+            onClick={() => onPageChange(pageIndex - 1)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === 0}
+          >
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Página anterior</span>
+          </Button>
+          <Button
+            onClick={() => onPageChange(pageIndex + 1)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pages <= pageIndex + 1}
+          >
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima página</span>
+          </Button>
+          <Button
+            onClick={() => onPageChange(pages - 1)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pages <= pageIndex + 1}
+          >
+            <ChevronsRight className="h-4 w-4" />
+            <span className="sr-only">Última página</span>
+          </Button>
         </div>
       </div>
-    </>
+    </div>
   )
 }
